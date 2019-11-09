a284b596a551138875c0529336a61062e6460a63
271 <admin@0x10f.dev>
2019-11-09 12:28:11 -0700

    Moved over to JSON for representation of matrices

--- cmd/matrices/main.go
+++ cmd/matrices/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
     "bufio"
+    "encoding/json"
     "fmt"
+    "io/ioutil"
     "log"
     "os"
     "strings"
@@ -35,6 +37,15 @@ var permutations = []map[string]int{
     {"A": 1, "B": 2, "C": 3, "D": 0,}, // D,A,B,C
 }
 
+type Matrix struct {
+    Zeros         int         `json:"zeros"`
+    Ones          int         `json:"ones"`
+    Ordering      int         `json:"ordering"`
+    ColorEncoding int         `json:"color_encoding"`
+    WidthEncoding int         `json:"width_encoding"`
+    Bits          [16][16]int `json:"bits"`
+}
+
 func main() {
     infile, err := os.Open("out.txt")
     if err != nil {
@@ -48,20 +59,11 @@ func main() {
         segments = append(segments, scanner.Text())
     }
 
-    outfile, err := os.Create("matrices.txt")
-    if err != nil {
-        log.Fatal("failed to create matrices file: ", err)
-    }
-
-    w := bufio.NewWriter(outfile)
-
-    min, max := 256, 0
+    matrices := make([]*Matrix, 0)
 
     // Color << 2 | Width ; Width << 2 | Color
     for order := 0; order < 2; order++ {
-        // Color encoding permutation
         for ce := 0; ce < 24; ce++ {
-            // Width encoding permutation
             for we := 0; we < 24; we++ {
                 encodedSegments := make([]int, 0, 64)
 
@@ -72,9 +74,9 @@ func main() {
                     var encoded int
                     switch order {
                     case 0:
-                        encoded = c << 2 | w
+                        encoded = c<<2 | w
                     case 1:
-                        encoded = w << 2 | c
+                        encoded = w<<2 | c
                     }
 
                     encodedSegments = append(encodedSegments, encoded)
@@ -85,47 +87,51 @@ func main() {
                 for ring := 0; ring < 16; ring++ {
                     for off := 0; off < 16; off++ {
                         // Bit position relative to the ring rotation.
-                        pos := ring * 16 + off
+                        pos := ring*16 + off
 
                         // Grab the segment that the bit is referencing to. This is just the bit position
                         // divided by 4.
                         encoded := encodedSegments[pos>>2]
 
                         // Starting from left to right, pick the bit from the encoded segment.
-                        bit := (encoded >> uint(0x3 - (pos & 0x3))) & 0x1
+                        bit := (encoded >> uint(0x3-(pos&0x3))) & 0x1
 
                         // Set the bit relative to the position in the ring it would be. Left to right encoding.
-                        rotated[ring] |= uint16(bit << (15 - uint((off + ring) % 16)))
+                        rotated[ring] |= uint16(bit << (15 - uint((off+ring)%16)))
                     }
                 }
 
                 zeros := 0
-
                 for _, v := range rotated {
                     zeros += strings.Count(fmt.Sprintf("%016b\n", v), "0")
                 }
 
-                if zeros > max {
-                    max = zeros
+                matrix := &Matrix{
+                    Zeros:         zeros,
+                    Ones:          256 - zeros,
+                    Ordering:      order,
+                    ColorEncoding: ce,
+                    WidthEncoding: we,
                 }
 
-                if zeros < min {
-                    min = zeros
+                for x, v := range rotated {
+                    for i := 0; i < 16; i++ {
+                        b := v >> uint(15-i) & 0x1
+                        matrix.Bits[x][i] = int(b)
+                    }
                 }
 
-                w.WriteString(fmt.Sprintf("%d:%d:%d/%d:%d\n", order, ce, we, zeros, 256-zeros))
-                w.WriteString("----------------\n")
-
-                for _, v := range rotated {
-                    w.WriteString(fmt.Sprintf("%016b\n", v))
-                }
-
-                w.WriteString("----------------\n")
-
-                w.Flush()
+                matrices = append(matrices, matrix)
             }
         }
-    }
 
-    fmt.Printf("%d:%d\n", min, max)
+        bs, err := json.Marshal(&matrices)
+        if err != nil {
+            log.Fatal("failed to marshal matrices: ", err)
+        }
+
+        if err := ioutil.WriteFile("matrices.json", bs, os.ModePerm); err != nil {
+            log.Fatal("failed to write matrices file: ", err)
+        }
+    }
 }
